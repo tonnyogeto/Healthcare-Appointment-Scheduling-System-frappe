41a92d86ccb53881d3bd6d8fbfe0111e01a61ff8
tonnyogeto <tonnymaishaogeto@gmail.com>
2025-08-28 11:53:39 +0300

    medical record doctype

---
+++ afya/afya/doctype/department/department.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, maisha and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Department(Document):
+	pass
--- afya/api.py
+++ afya/api.py
@@ -5,3 +5,16 @@ def get_doctor_leave(doctor):
     doc = frappe.get_doc("Doctor", doctor)
     return doc.leave_days
 
+def add_doctor_leave(doctor, leave_type, from_date, to_date):
+    doc = frappe.get_doc("Doctor", doctor)
+    doc.append("leave_days", {
+        "leave_type": leave_type,
+        "from_date": from_date,
+        "to_date": to_date,
+        "leave_status": "Approved"
+    })
+    doc.save()
+    return {"message": "Leave added successfully"}
+
+
+
